189fd7f7cdfa747de6c4a26017aa3daf8d2df0fb
coder-Retro <hasnainqadri9c@gmail.com>
2026-08-03 22:18:07 +0500

    Add: Restructures

--- lec5_Functions_And_Methods/Lecture/script.js
+++ lec5_Functions_And_Methods/Lecture/script.js
@@ -1,27 +1,30 @@
 // Call Back Function
 const arr = [1, 2, 3, 4, 5];
 
+
+// ForEach (performs a function for each element)
 arr.forEach((val) => {
     console.log(val);
 });
 
-// Map
+// Map (returns a new array by performing a function on each element)
 const squares = arr.map((val) => {
     return val**2;
 });
 
 console.log(squares);
 
-// Filter
+// Filter (returns a new array by using elements that pass a condition)
 const odds = arr.filter((val) => {
     return val%2;
 });
 
 console.log(odds);
 
-// Reduce
+// Reduce (returns a single answer developed from all elements of array)
 const nums = [1, 2, 3, 5, 4];
 const sum = nums.reduce((prev, curr) => {
     return (prev>curr)?prev:curr;
 });
+
 console.log(sum);

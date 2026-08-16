682cc636093eb1fc1afdde3cd0549c5772ed2b0e
coder-Retro <hasnainqadri9c@gmail.com>
2026-08-16 20:22:00 +0500

    Fix: Typos

--- lec5_Functions_And_Methods/Functions_And_Methods.js
+++ lec5_Functions_And_Methods/Functions_And_Methods.js
@@ -9,26 +9,26 @@ are called callBack Functions
 
 // ForEach (performs a function for each element)
 arr.forEach((val) => {
-    console.log(val);
+  console.log(val);
 });
 
 // Map (returns a new array by performing a function on each element)
 const squares = arr.map((val) => {
-    return val**2;
+  return val ** 2;
 });
 
 console.log(squares);
 
 // Filter (returns a new array by using elements that pass a condition)
 const odds = arr.filter((val) => {
-    return val%2;
+  return val % 2;
 });
 
 console.log(odds);
 
 // Reduce (returns a single answer developed from all elements of array)
 const max = arr.reduce((prev, curr) => {
-    return (prev>curr)?prev:curr;
+  return prev > curr ? prev : curr;
 });
 
 console.log(max);

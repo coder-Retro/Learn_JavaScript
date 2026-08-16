dddd63f5bf49e51c5e86f014419d21f044e4ef9c
coder-Retro <hasnainqadri9c@gmail.com>
2026-08-16 20:19:33 +0500

    Fix: Typos

--- lec1_Variables_And_DataTypes/Q2/script.js
+++ lec1_Variables_And_DataTypes/Q2/script.js
@@ -1,13 +1,13 @@
 const instagramProfile = {
-    userName : "m_hasna1n",
-    isVerified : false,
-    isFollowed : false,
-    posts : 0,
-    followers : 20,
-    following : 25,
-    displayName : "Kaido",
-    threadName : "m_hasna1n",
-    bio : "Eat Sleep Code, repeat"
+  userName: "m_hasna1n",
+  isVerified: false,
+  isFollowed: false,
+  posts: 0,
+  followers: 20,
+  following: 25,
+  displayName: "Kaido",
+  threadName: "m_hasna1n",
+  bio: "function repeat() { eat(); sleep(); code(); repeat(); }",
 };
 
 console.log(instagramProfile);
